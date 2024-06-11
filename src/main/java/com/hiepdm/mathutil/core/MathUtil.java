663196c0fd24a4e97869c584a7280b484cb22d2f
Hiep <doanmanhhiep.2003@gmail.com>
2024-06-11 10:01:32 +0700

    update main code to use recursion

--- src/main/java/com/hiepdm/mathutil/core/MathUtil.java
+++ src/main/java/com/hiepdm/mathutil/core/MathUtil.java
@@ -11,17 +11,18 @@ package com.hiepdm.mathutil.core;
 public class MathUtil {
     
     public static long getFactorial(int n) {
-        long product = 1;
         if (n < 0 || n > 20) {
             throw new IllegalArgumentException("Invalid n, n must be between 0 to 20");
         }
-        if (n == 0) {
+        if (n == 0 || n == 1) {
             return 1;
-        }
-        for (int i = 1; i <= n; i++) {
-            product *= i;
-        }
-        return product;
+        }    
+        return n * getFactorial(n - 1);
+        //recursion
     }
+    
+    //5! = 1.2.3.4.5
+    //   = 4!.5
+    //n! = n x (n - 1)! 
     
 }
